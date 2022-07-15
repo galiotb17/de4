c5df9eb96b21cba059e13896f2dcb6c05af23a2c
galiotb17 <nguyenxuanthao1401@gmail.com>
2022-07-15 09:57:03 +0700

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,30 @@
 $(document).ready(function () {
+    $('.header-bar').on('click', function (e) {
+        $('.header-navbar').addClass('active');
+        $('body').addClass('stop-scrolling');
+    }); 
     $('.header-navbar').on('click', function (e) {
         if ($(e.target).closest('.header-navbar ul').length <= 0) {
             $('.header-navbar').removeClass('active');
+            $('body').removeClass('stop-scrolling');
         }
+    });    
+    $('.client-say-slick').slick({
+        dots: true, 
+        arrows: false, 
+    });
+    $('.product-tab a').on('click', function (e) {
+        const tab_target = $(this).attr('tab-target');
+        $('.product-list').removeClass('active');
+        $('.product-tab a').removeClass('active');
+        $(this).addClass('active');
+        $(tab_target).addClass('active');
+    });
+    $('.site-footer-nav_heading').on('click', function (e) {
+        const footer_list = $('.site-footer-nav_heading').next();
+        footer_list.removeClass('active');
+        const this_list = $(this).next();
+        this_list.addClass('active');
     });
 
 });
